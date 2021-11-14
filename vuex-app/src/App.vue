a030e692ac21512f1684219f82fdb058e011c197
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-11-14 20:30:23 +0300

    create mapState

---
+++ vuex-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="up">
+      {{title}}
+    </div>
+    <div class="down">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+
+export default {
+  name: 'App',
+  computed:mapState([
+    'title',
+  ])
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
