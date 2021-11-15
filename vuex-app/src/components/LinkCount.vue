f2bd68dee77c80a13f7c0037235c4db65c71f940
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-11-16 02:08:18 +0300

    create removeAllLink

--- vuex-app/src/components/LinkCount.vue
+++ vuex-app/src/components/LinkCount.vue
@@ -2,22 +2,46 @@
   <div>
       <h1>Links Count Component</h1>
       <p>Number Of Active Links: {{linkCount}}</p>
+      <button v-on:click="deleteLinks">Delete All Links</button>
+      <p>{{msg}}</p>
   </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex';
+import {mapGetters,mapActions,mapMutations} from 'vuex';
 
 export default {
     name:'LinkCount',
+    data(){
+        return{
+            msg:''
+        }
+    },
     computed:{
         ...mapGetters([
             'linkCount'
         ])
+    },
+    methods:{
+        ...mapMutations(['REMOVE_ALL']),
+        ...mapActions(['deleteAll']),
+        deleteLinks(){
+            this.deleteAll().then(()=>{
+                this.msg="Deleted All Links"
+            })
+        }
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+button{
+    padding: 10px;
+    margin-top: 30px;
+    background-color: rgb(139, 131, 131);
+    width: 100%;
+    border: 1px solid lightgreen;
+    outline: 0;
+    cursor: poiter;
+}
 </style>
--- vuex-app/src/store/store.js
+++ vuex-app/src/store/store.js
@@ -23,11 +23,22 @@ export const store = new Vuex.Store({
         },
         REMOVE_LINK:(state,link)=>{
             state.links.splice(link,1)
+        },
+        REMOVE_ALL:(state)=>{
+            state.links=[]
         }
     },
     actions:{
         deleteLink:(context,link)=>{
             context.commit("REMOVE_LINK",link)
+        },
+        deleteAll({commit}){
+            return new Promise((resolve)=>{
+                setTimeout(()=>{
+                    commit('REMOVE_ALL')
+                    resolve()
+                },5000)
+            })
         }
     }
 })
